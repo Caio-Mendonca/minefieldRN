bf8229341d0711b7c70afaf9be7deb7f5941a0b7
Caio <caioeduardojm4@gmail.com>
2022-03-08 09:35:05 -0300

    rewrite

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
   Text,
   View,
 } from 'react-native';
-
 import params from './src/params';
-
+import Field from './src/components/field';
 export default class App extends Component{
   render(){
     return(
       <View>
+        
         <Text>Tamanho:{params.getRowsAmount()}X {params.getColumnsAmount()}</Text>
+        <Field></Field>
       </View>
     )
   }
